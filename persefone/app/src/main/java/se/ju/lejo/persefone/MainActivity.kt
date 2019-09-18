c18e1eae9477367988e136dae5e0eacaae1cd779
JonnaLennartsson <43805911+JonnaLennartsson@users.noreply.github.com>
2019-09-18 13:50:58 +0200

    ADD: started projeckt in Android Studio

---
+++ persefone/app/src/main/java/se/ju/lejo/persefone/MainActivity.kt
@@ -0,0 +1,12 @@
+package se.ju.lejo.persefone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
