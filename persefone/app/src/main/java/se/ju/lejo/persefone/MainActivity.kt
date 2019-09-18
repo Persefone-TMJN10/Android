90ec72840ed7511e1a0ab0bd60c803864926f482
Dasro <maxx_b@hotmail.com>
2019-09-18 15:57:19 +0200

    ADD: Added TimerFragment to supportFragmentManager instead of layout

--- persefone/app/src/main/java/se/ju/lejo/persefone/MainActivity.kt
+++ persefone/app/src/main/java/se/ju/lejo/persefone/MainActivity.kt
@@ -2,11 +2,27 @@ package se.ju.lejo.persefone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import se.ju.lejo.persefone.Fragments.TimerFragment
 
 class MainActivity : AppCompatActivity() {
+
+    var timerFragment: TimerFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        constructFragments()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.root_layout, timerFragment as Fragment, "timer")
+            .commit()
+
+    }
+
+    private fun constructFragments() {
+        timerFragment = TimerFragment()
     }
 }
