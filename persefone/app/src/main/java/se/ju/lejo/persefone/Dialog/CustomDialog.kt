4b292955983a42ce8a560c87b7b0e375b8ef88a8
Joakim <brjo1621@student.ju.se>
2019-09-24 15:35:40 +0200

    ADD class for customising and sending notifications to user. The notification comes in the shape of a dialog which can be customised with a title and a message.

---
+++ persefone/app/src/main/java/se/ju/lejo/persefone/Dialog/CustomDialog.kt
@@ -0,0 +1,23 @@
+package se.ju.lejo.persefone.Dialog
+
+import android.app.AlertDialog
+import android.content.Context
+
+class CustomDialog(_title: String, _message: String, currentActivity: Context?) {
+
+    private val builder = AlertDialog.Builder(currentActivity)
+    private var dialog: AlertDialog? = null
+
+    init {
+        builder.setTitle(_title)
+        builder.setMessage(_message)
+        builder.setPositiveButton("Got it!") { dialog, which ->
+            //if we want to add something after user pressed "Got it!"
+        }
+        dialog = builder.create()
+    }
+
+    fun show() {
+        dialog?.show()
+    }
+}
