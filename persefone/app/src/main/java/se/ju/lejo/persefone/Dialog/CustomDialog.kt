1cd33b453678340fca6c0eea1b6fc48ef43e3c89
Joakim <brjo1621@student.ju.se>
2019-09-24 16:45:47 +0200

    ADD vibration when notification comes

--- persefone/app/src/main/java/se/ju/lejo/persefone/Dialog/CustomDialog.kt
+++ persefone/app/src/main/java/se/ju/lejo/persefone/Dialog/CustomDialog.kt
@@ -2,11 +2,16 @@ package se.ju.lejo.persefone.Dialog
 
 import android.app.AlertDialog
 import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v4.app.Fragment
 
 class CustomDialog(_title: String, _message: String, currentActivity: Context?) {
 
     private val builder = AlertDialog.Builder(currentActivity)
     private var dialog: AlertDialog? = null
+    private var activityContext: Context? = currentActivity
 
     init {
         builder.setTitle(_title)
@@ -18,10 +23,20 @@ class CustomDialog(_title: String, _message: String, currentActivity: Context?) 
     }
 
     fun show() {
+        vibratePhone(activityContext)
         dialog?.show()
     }
 
     fun dismiss() {
         dialog?.dismiss()
+    }
+
+    fun vibratePhone(currentActivity: Context?) {
+        val vibrator = currentActivity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(200)
+        }
     }
 }
