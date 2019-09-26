c1fa9099f166e77c36646b6b3681aaf8b531451c
Dasro <maxx_b@hotmail.com>
2019-09-26 15:27:57 +0200

    REFACTOR: Changed BluetoothHandler from class to object

--- persefone/app/src/main/java/se/ju/lejo/persefone/Bluetooth/BluetoothHandler.kt
+++ persefone/app/src/main/java/se/ju/lejo/persefone/Bluetooth/BluetoothHandler.kt
@@ -5,65 +5,55 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import se.ju.lejo.persefone.MainActivity
 
-class BluetoothHandler {
+object BluetoothHandler {
+    
+    private val adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-    private var activity: MainActivity? = null
+    fun toggleBluetooth() {
 
-    constructor(activity: MainActivity) {
-        this.activity = activity
+        if(adapter!!.isEnabled) {
+            adapter.disable()
+        }
+        else {
+            adapter.enable()
+        }
+
     }
 
-    companion object {
+    fun toggleDiscovery() {
 
-        private val adapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
-        fun toggleBluetooth() {
-
-            if(adapter!!.isEnabled) {
-                adapter.disable()
-            }
-            else {
-                adapter.enable()
-            }
-
+        if(adapter.isDiscovering) {
+            adapter.cancelDiscovery()
+        }
+        else {
+            adapter.startDiscovery()
         }
 
-        fun toggleDiscovery() {
+    }
 
-            if(adapter.isDiscovering) {
-                adapter.cancelDiscovery()
-            }
-            else {
-                adapter.startDiscovery()
-            }
+    fun isEnabled(): Boolean {
+        return adapter.isEnabled
+    }
 
-        }
+    fun isDiscovering(): Boolean {
+        return adapter.isDiscovering
+    }
 
-        fun isEnabled(): Boolean {
-            return adapter.isEnabled
-        }
+    fun confirmBluetoothPermissions(activity: Activity) {
 
-        fun isDiscovering(): Boolean {
-            return adapter.isDiscovering
-        }
-
-        fun confirmBluetoothPermissions(activity: Activity) {
-
-            var permissionCheck = activity!!.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
-            permissionCheck += activity!!.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
+        var permissionCheck = activity!!.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
+        permissionCheck += activity!!.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
 
 
-            if(permissionCheck != 0){
+        if(permissionCheck != 0){
 
-                activity!!.requestPermissions(
-                    Array<String>(2) {
-                        Manifest.permission.ACCESS_FINE_LOCATION;
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    },
-                    1001
-                )
-
-            }
+            activity!!.requestPermissions(
+                Array<String>(2) {
+                    Manifest.permission.ACCESS_FINE_LOCATION;
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                },
+                1001
+            )
 
         }
 
